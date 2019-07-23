af0b1da89b23febc940d1fe7da38c468a939bdda
TJ <philosophy.cheer@gmail.com>
2019-07-23 16:12:25 -0400

    Created and continued to update the API

---
+++ app/main/api/continents.py
@@ -0,0 +1,90 @@
+# Pick like 3 countries from each continent and do the following:
+
+testingAPI = [
+    {"Id": 0},
+    {"Continent": "Asia"},
+    {"Taboos":
+        {"China": "Shoes and pears are inappropriate gifts in Chinese culture. They signify divorce or separation between a married couple. Clocks and umbrellas are also inappropriate gifts because they imply death and ending.",
+
+         "India": "Leather and meat products are inappropriate in Indian cultures because cows are a sacred animal in Hinduism.",
+
+         "South Korea": "Don’t discuss South Korean or North Korean politics, Socialism, Communism, and the Korean War"},
+     },
+    {"Customs":
+        {"China": "The largest festival — also called the Spring Festival — marks the beginning of the Lunar New Year. It falls between mid-January and mid-February and is a time to honor ancestors. During the 15-day celebration, the Chinese do something every day to welcome the new year, such as eat rice congee and mustard greens to cleanse the body, according to the University of Victoria. The holiday is marked with fireworks and parades featuring dancers dressed as dragons.",
+
+         "India": "Diwali is the largest and most important holiday to India, according to National Geographic. It is a five-day festival known as the festival of lights because of the lights lit during the celebration to symbolize the inner light that protects them from spiritual darkness.",
+
+         "South Korea": "In South Korea names operate in the reverse of Western cultures; Family name (surname), a second family name shared by all of that generation, and finally their given name. It is considered very impolite to address a Korean with his or her given name. They should be addressed using their professional titles, or Mr, Mrs Etc, until permission is given otherwise."},
+     },
+    {"Suggestions":
+        {"China": [
+            "Beijing, The Great Wall of China, Zhangjiajie National Forest Park, Huanglong National Scenic Reserve, Shanghai, Zhangye Danxia Landform, Yangshuo, Huashan's Plank Trail"],
+         "India": [
+            "Varanasi, Taj Mahal, The Taj Mahal, Anjuna / Vagator, Rann of Kutch, Tawang, Kaziranga National Park, Darjeeling, BR Hills"],
+         "South Korea": [
+            "Busan, Jinhae, Seoul, Gyeongju, Boseong, Bukchon Hanok Village, Jeju, Yeongam Gurim Village"],
+         }
+     },
+
+    {"Id": 1},
+    {"Continent": "Antarctica"},
+    {"Taboos":
+     {"Taboos": "Antarctica is a demilitarized zone. This means that no military activity can take place on the territory, including carrying out maneuvers and establishing military bases. In addition, no firearms or explosive devices are allowed without a special dispensation."},
+     },
+    {"Customs":
+        {"Customs": "Almost certainly the most widespread 'custom' in Antarctica is the Polar Plunge it revolves around being scarily cold, but only briefly. The seas around Antarctica vary from about 28.4F to 35.6F throughout the year, at 28.4F the sea freezes, the salt content makes this happen below 32F, so Antarctic sea water is often as cold as it is possible for water to get, especially in the winter months. Jumping in the sea wearing swimming attire is a surprisingly popular thing to do. Originally something that was done by members of scientific bases particularly at Midwinter or Christmas, this has spread across the continent and now happens far more often as shorter term visitors in the summer want to have a go too."}
+     },
+    {"Suggestions":
+        {"Suggestions": "Look but don’t touch the wildlife, Do not be a litterbug, Do not disturb artifacts, Don't be careless, Don’t travel without a survival kit, Don't expose your skin, Don’t take children to the Antarctic!"}
+     },
+
+    {"Id": 2},
+    {"Continent": "Africa"},
+    {"Taboos":
+        {"Egypt": "Egypt is a country full of mystery, which attracts numerous tourists. It’s an Arab Muslim country and the Egyptians tend to be very conservative with their own unique living habits, especially in rural areas, you will have to respect and comply with Muslim customs and traditions when traveling in Egypt. Do not wear sleeveless tops and short skirts or shorts on the streets or in markets. Do not eat communal food with your left hand, Do not offer alcohol or pork, Do not touch during a conversation",
+
+         "Nigeria": "It is forbidden to kill a snakes. It is forbidden to commit suicide. The parents/family of the person would be made to pay the fine. It is a taboo for pregnant women to go near corpses. In many cultures of Nigeria, it is forbidden to give or receive anything with your left hand.",
+
+         "South Africa": "It’s impolite to point with your index finger so use an open hand. Talking with your hands in your pockets is considered rude. When passing through a doorway, it’s customary for men to precede women. Don’t initiate or participate in racist or sexist conversations should the topic arise. The 'V' or peace sign is the same as giving 'someone the finger', and it’s usually punctuated by an upward thrust of the hand"},
+     },
+    {"Customs":
+        {"Egypt": "Those who want wealth and prosperity to come to their house, bury a weasel at the doorstep. Egyptians believe that burying a weasel at their doorstep will allow them to gain a lot of money and have a better life.",
+
+         "Nigeria": "The most common greeting is a handshake with a warm, welcoming smile. Men may place their left hand on the other person’s shoulder while shaking hands. Smiling and showing sincere pleasure at meeting the person is important. As in the rest of Africa, it is rude to rush the greeting process. You must take the time to ask about the person’s health, the health of their family, or other social niceties. A Nigerian generally waits for the woman to extend her hand. Address people initially by their academic, professional or honorific title and their surname. Always wait until invited before using someone’s first name. When greeting someone who is obviously much older, it is a sign of respect and deference to bow the head.",
+
+         "South Africa": "There are several greeting styles in South Africa depending upon the ethnic heritage of the person you are meeting. When dealing with foreigners, most South Africans shake hands while maintaining eye contact and smiling. Some women do not shake hands and merely nod their head, so it is best to wait for a woman to extend her hand. Men may kiss a woman they know well on the cheek in place of a handshake. Greetings are leisurely and include time for social discussion and exchanging pleasantries."},
+     },
+    {"Suggestions":
+        {"Egypt": ["Pyramids of Giza, Luxor's Karnak Temple and the Valley of the Kings, Islamic Cairo, Aswan, Feluccas on the Nile at Aswan, Abu Simbel."],
+         "Nigeria": ["Osun-Osogbo Sacred Grove, Abuja Airplane House, Lekki Conservation Center, Makoko Floating School"],
+         "South Africa": ["Kirstenbosch National Botanical Garden, Constitution Hill, Cape of Good Hope, Kruger National Park, Apartheid Museum"],
+         }
+     },
+
+    {"Id": 3},
+    {"Continent": "Europe"},
+    {"Taboos":
+        {"Spain": "",
+
+         "Finland": "",
+
+         "Bosnia and Herzegovina": ""},
+     },
+    {"Customs":
+        {"Spain": "",
+
+         "Finland": "",
+
+         "Bosnia and Herzegovina": ""},
+     },
+    {"Suggestions":
+        {"Spain": [""],
+         "Finland": [""],
+         "Bosnia and Herzegovina": [""],
+         }
+     },
+]
+
+
+# print(testingAPI)
